fde559ea44078c83eeb02b12c32d1cfbd29205b4
unknown <kheronn@gmail.com>
2023-08-02 22:02:46 -0300

    feature: createUsuario error not save in API

--- src/app/components/admin/usuario/usuario.component.ts
+++ src/app/components/admin/usuario/usuario.component.ts
@@ -2,22 +2,62 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsuariosService } from 'src/app/services/usuarios.service';
 import { Usuario } from 'src/app/models/usuario.model';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-usuario',
   standalone: true,
-  imports: [CommonModule, MatTableModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+  ],
   templateUrl: './usuario.component.html',
   styleUrls: ['./usuario.component.scss'],
 })
 export class UsuarioComponent implements OnInit {
   usuarioService = inject(UsuariosService);
   usuarios: Usuario[] = [];
-  colunas: string[] = ["nome", "email"];
+  colunas: string[] = ['nome', 'email'];
+
+  usuario: Usuario = {};
+
+  form!: FormGroup;
+  fb = inject(FormBuilder);
 
   ngOnInit(): void {
     this.carregaUsuarios();
+    this.createForm();
+  }
+
+  save() {
+    this.usuarioService.create(this.form.value);
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      id: [''],
+      nome: ['', [Validators.required, Validators.min(5)]],
+      email: ['', [Validators.required, Validators.email]],
+    });
   }
 
   carregaUsuarios() {
@@ -25,4 +65,6 @@ export class UsuarioComponent implements OnInit {
       .getUsuarios()
       .subscribe((dados) => (this.usuarios = dados));
   }
+
+  novoUsuario() {}
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -46,9 +46,9 @@ export class LoginComponent implements OnInit {
       password: ['', Validators.required],
     });
 
-    this.ufService
-              .getUfs()
-              .subscribe((estados) => console.log(estados));
+    // this.ufService
+    //           .getUfs()
+    //           .subscribe((estados) => console.log(estados));
   }
 
   login() {
